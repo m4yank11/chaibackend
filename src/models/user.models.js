d8abbebfcf360601e1555d113b9a3c796472400b
Mayank Raj Gupta <mayankrajgupta3112@gmail.com>
2025-03-13 11:25:04 +0530

    setting up cloudinary and multer middleware

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -28,11 +28,11 @@ const userSchema = new Schema(
             index: true
         },
         avatar: {
-            type: String, // cloudinary url use krenge: its like aws but free
+            type: String, // cloudinary url use krenge
             required: false
         },
         coverImage: {
-            type: String, // cloudinary url use krenge: its like aws but free
+            type: String, // cloudinary url use krenge
         },
         password: {
             type: String,
@@ -63,19 +63,17 @@ userSchema.pre("save", async function(next) {
 // Explanation:
 // 	1.	userSchema.pre("save", async function(next) {...})
 // 	•	This is a pre-save middleware that runs before saving a document (user) to the database.
-// 	•	It takes next as a parameter to move to the next mi ddleware in the chain.
+// 	•	It takes next as a parameter to move to the next middleware in the chain.
 // 	2.	if (this.isModified("password")) {...}
 // 	•	this.isModified("password") checks whether the password field has been modified.
 // 	•	This ensures that password hashing only occurs when it is newly created or updated, preventing unnecessary re-hashing.
-// 	3.	this.password = await bcrypt.hash(this.password, 10);
-// 	•	Uses bcrypt to hash the password 
 
 
 userSchema.methods.isPasswordCorrect = async function(password){
     return await bcrypt.compare(password, this.password)
 }
 
-userSchema.methods.generateAccessToken = function(){ //This function generateAccessToken is a method of the userSchema in a MongoDB-based Node.js application using JWT (JSON Web Token).generates a JWT token for a user when called.
+userSchema.methods.generateAccessToken = function(){ 
     return jwt.sign(
         {
             _id: this.id,
@@ -89,7 +87,7 @@ userSchema.methods.generateAccessToken = function(){ //This function generateAcc
         }
     )
 }
-
+    //      This function generateAccessToken is a method of the userSchema in a MongoDB-based Node.js application using JWT (JSON Web Token).generates a JWT token for a user when called.
     //      The token includes the user’s _id, email, username, and fullName as payload.
 	// •	It is signed using a secret key (process.env.ACCESS_TOKEN_SECRET) stored in environment variables.
 	// •	The token has an expiry time defined in process.env.ACCESS_TOKEN_EXPIRY.
